5af05a0cd5a54b0cc3f7b635f6a933a5ed25c620
whi724 <100496549+whi724@users.noreply.github.com>
2022-02-27 04:46:30 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,30 +1,87 @@
 import asyncio
 import websockets
+import json
 
-connected = dict()
 
-async def server(websocket, path):
-    # Register.
+class_dict = {}
 
-    #connected.add(websocket)
-    #print(connected)
-    
-    try:
-        async for message in websocket:
-            if not websocket in connected:
-                connected[websocket] = message
-            #print('asdfasdfasdfasdfasdf')
-            #for conn in connected:
-                #if conn != websocket:
-                #await conn.send(message)
-            websockets.broadcast(set(connected.keys()), f'Got a new MSG FOR YOU from {connected[websocket]}: {message}')
-            #await websocket.wait_closed()
-    finally:
-        # Unregister.
-        del connected[websocket]
-    
 
-start_server = websockets.serve(server, "localhost", 5005)
+async def filter_or_create(websocket):
+    async for messages in websocket:
+        key = messages[:6]
+        messages = messages[9:].split(';;;')
+        object_list = []
+        if key == 'FILTER':
+            print(messages)
+            if len(messages) == 1:
+                for keys, values in class_dict.items():
+                    if messages[0] == keys:
+                        for num, lists in class_dict[messages[0]].items():
+                            # print(lists)
+                            for posts in lists:
+                                # print(f"FILTER> {messages[0] + ';;;' + num + ';;;' + posts}")
+                                # await websocket.send(messages[0] + ';;;' + num + ';;;' + posts)
+                                object_list.append({'school': messages[0], 'course_code': num, 'post': posts})
+                            # break
+                await websocket.send(json.dumps(object_list))
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+            elif len(messages) == 2:
+                for keys, values in class_dict.items():
+                    if messages[0] == keys:
+                        for classes, posts in values.items():
+                            if classes == messages[1]:
+                                for post in posts:
+                                    # print(f"> {messages[0]+ ';;;' + messages[1] + ';;;' + post}")
+                                    # await websocket.send(messages[0]+ ';;;' + messages[1] + ';;;' + post)
+                                    object_list.append({'school': messages[0], 'course_code': classes, 'post': posts})
+                
+                await websocket.send(json.dumps(object_list))
+
+            await websocket.send('BREAK')
+
+        elif key == 'CREATE':
+            post = []
+            if messages[0] in class_dict.keys():
+                if messages[1] in class_dict[messages[0]].keys():
+                    class_dict[messages[0]][messages[1]].append(messages[2])
+                    print(f'CREATED> {class_dict}')
+                    
+                else:
+                    class_dict[messages[0]][messages[1]] = [messages[2]]
+                    print(f'CREATED> {class_dict[messages[0]][messages[1]]}')
+                    
+            else:
+                class_dict[messages[0]] = {messages[1]: [messages[2]]}
+                print(f'CREATED> {class_dict[messages[0]]}')
+                
+            for school, values in class_dict.items():
+                # print(school)
+                for classes, lists in values.items():
+                    # print(classes)
+                    for posts in lists:
+                        # print(f"SENT> {school + ';;;' + classes + ';;;' + posts}")
+                        # await websocket.send(school + ';;;' + classes + ';;;' + posts)
+                        post.append({'school': school, 'course_code': classes, 'post': posts})
+
+            await websocket.send(json.dumps(post))
+            await websocket.send('BREAK')
+
+        # else:
+        #     for school, values in class_dict.items():
+        #         print(school)
+        #         for classes, lists in values.items():
+        #             print(classes)
+        #             for posts in lists:
+        #                 print(f"SENT> {school + ';;;' + classes + ';;;' + posts}")
+        #                 await websocket.send(school + ';;;' + classes + ';;;' + posts)
+            
+        #     await websocket.send('BREAK')
+
+
+async def main():
+    async with websockets.serve(filter_or_create, "localhost", 8765):
+        await asyncio.Future()  # run forever
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
